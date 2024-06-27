cea23c8e876a0fe14a1e4bd7da0a2fb80eeeb4c4
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-06-27 21:19:03 +0530

    Update block.js

--- block.js
+++ block.js
@@ -13,3 +13,37 @@ else{
 }
 var abi=[];
 var contract=new web3.eth.Contract(abi,address);
+
+
+
+function deposite(){
+var inputval= document.getElementById("amount").value;
+web3.eth.getAccounts().then(function(account){
+  return contract.methods.deposite_money(inputval).send({from: account[0]});
+}).then(function(tmp){
+  $("#amount").val("");
+show_balance();
+}).catch(function(tmp){
+  alert(tmp);
+})
+}
+
+
+
+function withdraw(){
+    var inputval= document.getElementById("amount").value;
+
+web3.eth.getAccounts().then(function(account){
+  return contract.methods.withdraw(inputval).send({from: account[0]});
+}).then(function(tmp){
+  $("#amount").val("");
+show_balance();
+}).catch(function(tmp){
+  alert(tmp);
+})
+}
+
+function show_balance(){
+  contract.methods.getBalance().call().then(function(balance){
+    $("balance").html(balance);
+  })
